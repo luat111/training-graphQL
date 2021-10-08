a4535619a9bff9503c4d9e1950ace6cfb6cee9dd
luat111 <nguyentrongluat1999@gmail.com>
2021-10-08 16:59:47 +0700

    remove dist

--- dist/schemas/book/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var queries_1 = require("./queries");
-var mutation_1 = require("./mutation");
-exports.default = {
-    BookQuery: queries_1.BookQuery,
-    BookMutation: mutation_1.BookMutation
-};
